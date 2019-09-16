90287fa73c77261cbe1fddaf2347e62ba5776f47
susautw <susautw@gmail.com>
2019-09-17 06:54:00 +0800

    refine code

--- script-path-fixer/__main__.py
+++ script-path-fixer/__main__.py
@@ -1,10 +1,11 @@
 import os
 import argparse
 import pathlib
+import sys
 
 
 def main():
-    args = get_args()
+    args = parse_args(sys.argv[1:])
 
     interpreter_path = pathlib.Path(args.interpreter)
     script_file = pathlib.Path(args.script)
@@ -15,24 +16,24 @@ def main():
     exit(exit_code)
 
 
-def get_args():
+def parse_args(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--interpreter', type=str, required=True)
     parser.add_argument('-s', '--script', type=str, required=True)
     parser.add_argument('va_list', nargs='*')
-    return parser.parse_args()
+    return parser.parse_args(argv)
 
 
 def get_script_path(interpreter_path: pathlib.Path, script_file: pathlib.Path):
     path = pathlib.Path()
     path = path / interpreter_path / script_file \
-        if is_interpreter_in_scripts(interpreter_path) \
+        if is_interpreter_in_scripts_directory(interpreter_path) \
         else path / interpreter_path / 'Scripts' / script_file
 
     return path
 
 
-def is_interpreter_in_scripts(interpreter_path: pathlib.Path):
+def is_interpreter_in_scripts_directory(interpreter_path: pathlib.Path):
     return interpreter_path.parts[-1] == 'Scripts'
 
 
